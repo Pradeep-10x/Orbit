81d22ecbe53ae0aae587af1cf2e54ddb1f94733e
Pradeep <pradeep2ndsingh@gmail.com>
2026-01-19 00:21:29 +0530

    port change

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 
 const server = http.createServer(app);
